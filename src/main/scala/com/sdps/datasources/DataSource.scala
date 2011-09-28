52fbc09104fae64d472204e8fffb3f81f607a6a7
Adam Jorgensen <adamj@sjsoft.com>
2011-09-28 10:55:02 +0200

    Partially documented Filters

--- src/main/scala/com/sdps/datasources/DataSource.scala
+++ src/main/scala/com/sdps/datasources/DataSource.scala
@@ -15,7 +15,33 @@ abstract class DataSource(val uri: URI) {
 
     def getItemsById(ids: Seq[JValue] = Nil, attributes: Seq[JString] = Nil): Seq[JObject]
 
-    def getItemsByFilter(filters: Seq[(JString, JString, JValue)] = Nil, attributes: Seq[JString] = Nil): Seq[JObject]
+    /**
+     * Filters following a fairly simple format. Each Filter consists of a tuple containing:
+     *
+     * - A JArray element used to locate the property on the to be checked on the item being filtered
+     * - A JString element used to determine the comparison operation to be used
+     * - A JValue element used to compare the property value against
+     *
+     *
+     * The JArray element follows a fairly simply format in that the only valid values to be contained within
+     * the JArray are JStrings and JInt values.
+     *
+     * A JString value is used to resolve a property name on the item being filtered and is applicable
+     * when the base item is a JObject.
+     *
+     * A JInt value is used to resolve a value by numeric index and is applicable when the base item
+     * is a JArray
+     *
+     *
+     * The JString element may be one of: > >= = <= < in or like with the the ! (not) suffix being
+     * valid for each of these.
+     *
+     *
+     * The JValue element may be any valid JValue sub-class for which a comparison option is valid
+     *
+     * TODO: Detail the manner in which the comparison operators interact with various values
+     */
+    def getItemsByFilter(filters: Seq[(JArray, JString, JValue)] = Nil, attributes: Seq[JString] = Nil): Seq[JObject]
 
     def addItems(items: Seq[JObject]): Seq[JValue]
 
