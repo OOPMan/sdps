129bc2eda846768ffc58da3d090728313d7aa66c
Adam Jorgensen <adamj@sjsoft.com>
2011-11-17 16:58:14 +0200

    Added all Double itemFilter tests. Two are broken for now.

--- src/test/scala/com/sdps/test/datasources/JSONObjectFileDataSourceTest.scala
+++ src/test/scala/com/sdps/test/datasources/JSONObjectFileDataSourceTest.scala
@@ -27,9 +27,9 @@ class JSONObjectFileDataSourceTest extends FunSuite with BeforeAndAfterAll {
     val jsonObjects = List(
         ("field1" -> "string1") ~ ("field2" -> 1) ~ ("field3" -> 1.0) ~ ("field4" -> true)  ~ ("field5" -> List(1,2,3)) ~ ("field6" -> ("a" -> 1) ~ ("b" -> 2) ~  ("c" -> 3)),
         ("field1" -> "string2") ~ ("field2" -> 2) ~ ("field3" -> 2.0) ~ ("field4" -> false) ~ ("field5" -> List(2,3,4)) ~ ("field6" -> ("a" -> 2) ~ ("b" -> 4) ~  ("c" -> 6)),
-        ("field1" -> "string3") ~ ("field2" -> 3) ~ ("field3" -> 4.0) ~ ("field4" -> true)  ~ ("field5" -> List(3,4,5)) ~ ("field6" -> ("a" -> 3) ~ ("b" -> 6) ~  ("c" -> 9)),
-        ("field1" -> "string4") ~ ("field2" -> 4) ~ ("field3" -> 5.0) ~ ("field4" -> false) ~ ("field5" -> List(4,5,6)) ~ ("field6" -> ("a" -> 4) ~ ("b" -> 8) ~  ("c" -> 12)),
-        ("field1" -> "string5") ~ ("field2" -> 5) ~ ("field3" -> 6.0) ~ ("field4" -> true)  ~ ("field5" -> List(5,6,7)) ~ ("field6" -> ("a" -> 5) ~ ("b" -> 10) ~ ("c" -> 15)),
+        ("field1" -> "string3") ~ ("field2" -> 3) ~ ("field3" -> 3.0) ~ ("field4" -> true)  ~ ("field5" -> List(3,4,5)) ~ ("field6" -> ("a" -> 3) ~ ("b" -> 6) ~  ("c" -> 9)),
+        ("field1" -> "string4") ~ ("field2" -> 4) ~ ("field3" -> 4.0) ~ ("field4" -> false) ~ ("field5" -> List(4,5,6)) ~ ("field6" -> ("a" -> 4) ~ ("b" -> 8) ~  ("c" -> 12)),
+        ("field1" -> "string5") ~ ("field2" -> 5) ~ ("field3" -> 5.0) ~ ("field4" -> true)  ~ ("field5" -> List(5,6,7)) ~ ("field6" -> ("a" -> 5) ~ ("b" -> 10) ~ ("c" -> 15)),
         ("name" -> "something different") ~ ("description" -> (None: Option[String])) ~ ("field3" -> 6.0)
     )
 
@@ -41,7 +41,13 @@ class JSONObjectFileDataSourceTest extends FunSuite with BeforeAndAfterAll {
     val filterForObjectsWithField2GreaterThanEqualTo3 = (JArray("field2" :: Nil), JString(">="), JInt(3)) :: Nil
     val filterForObjectsWithField2GreaterThan3 = (JArray("field2" :: Nil), JString(">"), JInt(3)) :: Nil
     val filterForObjectsWithField2NotEqualTo3 = (JArray("field2" :: Nil), JString("!="), JInt(3)) :: Nil
-    //TODO: Float ItemFilter
+    // Float ItemFilter
+    val filterForObjectsWithField2LessThan3point0 = (JArray("field3" :: Nil), JString("<"), JDouble(3.0)) :: Nil
+    val filterForObjectsWithField2LessThanOrEqualTo3point0 = (JArray("field3" :: Nil), JString("<="), JDouble(3.0)) :: Nil
+    val filterForObjectsWithField2EqualTo3point0 = (JArray("field3" :: Nil), JString("=="), JDouble(3.0)) :: Nil
+    val filterForObjectsWithField2GreaterThanEqualTo3point0 = (JArray("field3" :: Nil), JString(">="), JDouble(3.0)) :: Nil
+    val filterForObjectsWithField2GreaterThan3point0 = (JArray("field3" :: Nil), JString(">"), JDouble(3.0)) :: Nil
+    val filterForObjectsWithField2NotEqualTo3point0 = (JArray("field3" :: Nil), JString("!="), JDouble(3.0)) :: Nil
     //TODO: Boolean ItemFilter
     //TODO: Object ItemFilter
     //TODO: Array ItemFilter
@@ -97,6 +103,32 @@ class JSONObjectFileDataSourceTest extends FunSuite with BeforeAndAfterAll {
         val items = dataSource.getItemsByFilter(filterForObjectsWithField2GreaterThan3)
         expect(2) { items.length }
         assert((for { (id, objekt) <- items } yield idObjectMap(id) == objekt) reduce { _ & _ }, "query for objects with field2 > 3 failed")
+    }
+
+    test("query for data with field2 < 3.0") {
+        val items = dataSource.getItemsByFilter(filterForObjectsWithField2LessThan3point0)
+        expect(2) { items.length }
+        assert((for { (id, objekt) <- items } yield idObjectMap(id) == objekt) reduce { _ & _ }, "query for objects with field2 < 3.0 failed")
+    }
+    test("query for data with field2 <= 3.0") {
+        val items = dataSource.getItemsByFilter(filterForObjectsWithField2LessThanOrEqualTo3point0)
+        expect(3) { items.length }
+        assert((for { (id, objekt) <- items } yield idObjectMap(id) == objekt) reduce { _ & _ }, "query for objects with field2 <= 3.0 failed")
+    }
+    test("query for data with field2 == 3.0") {
+        val items = dataSource.getItemsByFilter(filterForObjectsWithField2EqualTo3point0)
+        expect(1) { items.length }
+        assert((for { (id, objekt) <- items } yield idObjectMap(id) == objekt) reduce { _ & _ }, "query for objects with field2 == 3.0 failed")
+    }
+    test("query for data with field2 >= 3.0") {
+        val items = dataSource.getItemsByFilter(filterForObjectsWithField2GreaterThanEqualTo3point0)
+        expect(4) { items.length }
+        assert((for { (id, objekt) <- items } yield idObjectMap(id) == objekt) reduce { _ & _ }, "query for objects with field2 >= 3.0 failed") //TODO: Fix
+    }
+    test("query for data with field2 > 3.0") {
+        val items = dataSource.getItemsByFilter(filterForObjectsWithField2GreaterThan3point0)
+        expect(3) { items.length }
+        assert((for { (id, objekt) <- items } yield idObjectMap(id) == objekt) reduce { _ & _ }, "query for objects with field2 > 3.0 failed") //TODO: Fix
     }
 
     test("filter written data") {
