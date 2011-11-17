d31473df84ae132c57e1b8ec1996ce4f4dd8edeb
Adam Jorgensen <adamj@sjsoft.com>
2011-11-17 11:09:44 +0200

    Added all Int itemFilter tests

--- src/test/scala/com/sdps/test/datasources/JSONObjectFileDataSourceTest.scala
+++ src/test/scala/com/sdps/test/datasources/JSONObjectFileDataSourceTest.scala
@@ -15,6 +15,7 @@ import com.sdps.datasources.JSONObjectFileDataSource
  * To change this template use File | Settings | File Templates.
  */
 
+//TODO: Split Test up by creating abstract test concrete sub-classes
 class JSONObjectFileDataSourceTest extends FunSuite with BeforeAndAfterAll {
 
     val tempFile = File.createTempFile("JSONObjectFileDataSourceTest", ".json")
@@ -23,19 +24,37 @@ class JSONObjectFileDataSourceTest extends FunSuite with BeforeAndAfterAll {
     var idObjectMap: Map[JValue, JValue] = Map()
     implicit val formats = DefaultFormats
 
-    case class ObjectA(field1: String, field2: Int, field3: Float, field4: Boolean, field5: List[Int])
-    case class ObjectB(name: String, description: Option[String], field3: Float)
-
     val jsonObjects = List(
-        ("field1" -> "string1") ~ ("field2" -> 1) ~ ("field3" -> 1.0) ~ ("field4" -> true) ~ ("field5" -> List(1,2,3)),
-        ("field1" -> "string2") ~ ("field2" -> 2) ~ ("field3" -> 2.0) ~ ("field4" -> false) ~ ("field5" -> List(2,3,4)),
-        ("field1" -> "string3") ~ ("field2" -> 3) ~ ("field3" -> 4.0) ~ ("field4" -> true) ~ ("field5" -> List(3,4,5)),
-        ("field1" -> "string4") ~ ("field2" -> 4) ~ ("field3" -> 5.0) ~ ("field4" -> false) ~ ("field5" -> List(4,5,6)),
-        ("field1" -> "string5") ~ ("field2" -> 5) ~ ("field3" -> 6.0) ~ ("field4" -> true) ~ ("field5" -> List(5,6,7)),
+        ("field1" -> "string1") ~ ("field2" -> 1) ~ ("field3" -> 1.0) ~ ("field4" -> true)  ~ ("field5" -> List(1,2,3)) ~ ("field6" -> ("a" -> 1) ~ ("b" -> 2) ~  ("c" -> 3)),
+        ("field1" -> "string2") ~ ("field2" -> 2) ~ ("field3" -> 2.0) ~ ("field4" -> false) ~ ("field5" -> List(2,3,4)) ~ ("field6" -> ("a" -> 2) ~ ("b" -> 4) ~  ("c" -> 6)),
+        ("field1" -> "string3") ~ ("field2" -> 3) ~ ("field3" -> 4.0) ~ ("field4" -> true)  ~ ("field5" -> List(3,4,5)) ~ ("field6" -> ("a" -> 3) ~ ("b" -> 6) ~  ("c" -> 9)),
+        ("field1" -> "string4") ~ ("field2" -> 4) ~ ("field3" -> 5.0) ~ ("field4" -> false) ~ ("field5" -> List(4,5,6)) ~ ("field6" -> ("a" -> 4) ~ ("b" -> 8) ~  ("c" -> 12)),
+        ("field1" -> "string5") ~ ("field2" -> 5) ~ ("field3" -> 6.0) ~ ("field4" -> true)  ~ ("field5" -> List(5,6,7)) ~ ("field6" -> ("a" -> 5) ~ ("b" -> 10) ~ ("c" -> 15)),
         ("name" -> "something different") ~ ("description" -> (None: Option[String])) ~ ("field3" -> 6.0)
     )
 
+    //TODO: String ItemFilter
+    // Int ItemFilter
+    val filterForObjectsWithField2LessThan3 = (JArray("field2" :: Nil), JString("<"), JInt(3)) :: Nil
+    val filterForObjectsWithField2LessThanOrEqualTo3 = (JArray("field2" :: Nil), JString("<="), JInt(3)) :: Nil
     val filterForObjectsWithField2EqualTo3 = (JArray("field2" :: Nil), JString("=="), JInt(3)) :: Nil
+    val filterForObjectsWithField2GreaterThanEqualTo3 = (JArray("field2" :: Nil), JString(">="), JInt(3)) :: Nil
+    val filterForObjectsWithField2GreaterThan3 = (JArray("field2" :: Nil), JString(">"), JInt(3)) :: Nil
+    val filterForObjectsWithField2NotEqualTo3 = (JArray("field2" :: Nil), JString("!="), JInt(3)) :: Nil
+    //TODO: Float ItemFilter
+    //TODO: Boolean ItemFilter
+    //TODO: Object ItemFilter
+    //TODO: Array ItemFilter
+
+    //TODO: String ContentFilter
+    //TODO: Int ContentFilter
+    //TODO: Float ContentFilter
+    //TODO: Boolean ContentFilter
+    //TODO: Object ContentFilter
+    //TODO: Array ContentFilter
+
+    //TODO: OrderBy
+    //TODO: itemRange
 
     test("read empty") {
         assert(dataSource.getItemsById().isEmpty, "dataSource was not empty")
@@ -54,11 +73,30 @@ class JSONObjectFileDataSourceTest extends FunSuite with BeforeAndAfterAll {
             "read data does not match written fixture")
     }
 
-    test("query written data") {
-        // TODO: Test ALL operators
-        assert(
-            (for { (id, objekt) <- dataSource.getItemsByFilter(filterForObjectsWithField2EqualTo3) } yield idObjectMap(id) == objekt) reduce { _ & _ },
-            "query for objects with field2 equal to 3 failed")
+    test("query for data with field2 < 3") {
+        val items = dataSource.getItemsByFilter(filterForObjectsWithField2LessThan3)
+        expect(2) { items.length }
+        assert((for { (id, objekt) <- items } yield idObjectMap(id) == objekt) reduce { _ & _ }, "query for objects with field2 < 3 failed")
+    }
+    test("query for data with field2 <= 3") {
+        val items = dataSource.getItemsByFilter(filterForObjectsWithField2LessThanOrEqualTo3)
+        expect(3) { items.length }
+        assert((for { (id, objekt) <- items } yield idObjectMap(id) == objekt) reduce { _ & _ }, "query for objects with field2 <= 3 failed")
+    }
+    test("query for data with field2 == 3") {
+        val items = dataSource.getItemsByFilter(filterForObjectsWithField2EqualTo3)
+        expect(1) { items.length }
+        assert((for { (id, objekt) <- items } yield idObjectMap(id) == objekt) reduce { _ & _ }, "query for objects with field2 == 3 failed")
+    }
+    test("query for data with field2 >= 3") {
+        val items = dataSource.getItemsByFilter(filterForObjectsWithField2GreaterThanEqualTo3)
+        expect(3) { items.length }
+        assert((for { (id, objekt) <- items } yield idObjectMap(id) == objekt) reduce { _ & _ }, "query for objects with field2 >= 3 failed")
+    }
+    test("query for data with field2 > 3") {
+        val items = dataSource.getItemsByFilter(filterForObjectsWithField2GreaterThan3)
+        expect(2) { items.length }
+        assert((for { (id, objekt) <- items } yield idObjectMap(id) == objekt) reduce { _ & _ }, "query for objects with field2 > 3 failed")
     }
 
     test("filter written data") {
