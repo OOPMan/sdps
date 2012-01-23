a7365ad4c4cf6c3e75f9fb190488944f8517236f
Adam Jorgensen <adamj@sjsoft.com>
2012-01-23 16:39:21 +0200

    Altered New to accept Any* rather than WithType*. This allows us to call New using the form New(classNameString)(constructorParametersList: _*) Added new implicit conversion method to handle converting List[Any] to List[WithType]

--- src/main/scala/com/sdps/utils/Reflection.scala
+++ src/main/scala/com/sdps/utils/Reflection.scala
@@ -10,16 +10,17 @@ object Reflection {
         clazz.asInstanceOf[Class[T]]
     }
 
-    def New[T <: AnyRef](className: String)(args: WithType*)(implicit classLoader: ClassLoader): T = {
+    def New[T <: AnyRef](className: String)(args: Any*)(implicit classLoader: ClassLoader): T = {
         val clazz: Class[T] = className
-        val argTypes = args map {
+        val argsWithType: List[WithType] = args +: Nil
+        val argTypes = argsWithType map {
             _.clazz
         } toArray
         val candidates = clazz.getConstructors filter {
             cons => matchingTypes(cons.getParameterTypes, argTypes)
         }
         require(candidates.length == 1, "Argument runtime types must select exactly one constructor")
-        val params = args map {
+        val params = argsWithType map {
             _.value
         }
         candidates.head.newInstance(params: _*).asInstanceOf[T]
@@ -43,6 +44,11 @@ object Reflection {
 
     case class RefWithType(anyRef: AnyRef, clazz: Class[_]) extends WithType {
         val value = anyRef
+    }
+    
+    implicit def ListAnyToListWithType(l: List[Any]): List[WithType] = l map {
+        case i: AnyRef => RefWithType(i, i.getClass)
+        case i: AnyVal => ValWithType(i, getType(i))
     }
 
     implicit def refWithType[T <: AnyRef](x: T) = RefWithType(x, x.getClass)
