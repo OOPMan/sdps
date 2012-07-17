1964cd9b32af814b37cb1454ca97bf492a2318c8
Adam Jorgensen <adamj@sjsoft.com>
2012-07-17 12:11:57 +0200

    Added JSONQueryAST

---
+++ src/main/scala/com/sdps/datasources/JSONQueryAST.scala
@@ -0,0 +1,36 @@
+package com.sdps.datasources
+
+import net.liftweb.json._
+
+object JSONQueryAST {
+    def parseJValue(input: JValue): JSONQueryAST = {
+        (input \ "#type", input \ "#value") match {
+            case (JString("nothing"), _) =>
+                JSONQueryASTNothing
+            case (JString("value"), value) =>
+                JSONQueryASTValue(value)
+            case (JString("property"), propertyArray: JArray) =>
+                val properties = propertyArray.arr map {
+                  case JString(s) => Left(s)
+                  case JInt(i) => Right(i)
+                }
+                JSONQueryASTProperty(properties)
+            case (JString("operation"), operandArray: JArray) =>
+                val operation = operandArray.arr.head match { case JString(s) => s }
+                val operands = operandArray.arr.tail map parseJValue
+                JSONQueryASTOperation(operation, operands)
+            case _ =>
+                JSONQueryASTValue(input)
+        }
+    }
+}
+
+class JSONQueryAST
+
+case object JSONQueryASTNothing extends JSONQueryAST
+
+case class JSONQueryASTValue(value: JValue) extends JSONQueryAST
+
+case class JSONQueryASTProperty(property: Seq[Either[String, BigInt]]) extends JSONQueryAST
+
+case class JSONQueryASTOperation(operation: String, operands: Seq[JSONQueryAST]) extends JSONQueryAST
