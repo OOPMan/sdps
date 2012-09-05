24cdd2db728cd369ea89b117b5481ad4cd9573de
Adam Jorgensen <adamj@sjsoft.com>
2012-09-05 16:10:14 +0200

    Implemented basic JSONQueryAST and parsing function to convert JValues to JSONQueryAST values, including conversion of nested JValues.

--- src/main/scala/com/sdps/datasources/JSONQueryAST.scala
+++ src/main/scala/com/sdps/datasources/JSONQueryAST.scala
@@ -3,36 +3,58 @@ package com.sdps.datasources
 import net.liftweb.json._
 
 object JSONQueryAST {
-    def parseJValue(input: JValue): JSONQueryAST = {
-        (input \ "#type", input \ "#value") match {
-            case (JString("nothing"), _) =>
-                JSONQueryASTNothing
-            case (JString("value"), value) =>
-                JSONQueryASTValue(value)
-            case (JString("property"), propertyArray: JArray) =>
-                val properties = propertyArray.arr map {
-                  case JString(s) => Left(s)
-                  case JInt(i) => Right(i)
-                }
-                JSONQueryASTProperty(properties)
-            case (JString("operation"), operandArray: JArray) =>
-                val (operation, operands) = input \ "#operation" match {
-                  case JString(s) => (s, operandArray.arr map parseJValue)
-                  case JNothing => (operandArray.arr.head match { case JString(s) => s }, operandArray.arr.tail map parseJValue)
-                }
-                JSONQueryASTOperation(operation, operands)
-            case _ =>
-                JSONQueryASTValue(input)
-        }
+
+    abstract class JSONQueryASTValue
+
+    case object JSONQueryASTNothing extends JSONQueryASTValue
+
+    case object JSONQueryASTNull extends JSONQueryASTValue
+
+    case class JSONQueryASTString(s: String) extends JSONQueryASTValue
+
+    case class JSONQueryASTDouble(num: Double) extends JSONQueryASTValue
+
+    case class JSONQueryASTInt(num: BigInt) extends JSONQueryASTValue
+
+    case class JSONQueryASTBool(value: Boolean) extends JSONQueryASTValue
+
+    case class JSONQueryASTField(name: String, value: JSONQueryASTValue) extends JSONQueryASTValue
+
+    case class JSONQueryASTObject(obj: List[JSONQueryASTField]) extends JSONQueryASTValue
+
+    case class JSONQueryASTArray(arr: List[JSONQueryASTValue]) extends JSONQueryASTValue
+
+    case class JSONQueryASTProperty(property: List[Either[String, BigInt]]) extends JSONQueryASTValue
+
+    case class JSONQueryASTOperation(operation: String, operands: List[JSONQueryASTValue]) extends JSONQueryASTValue
+
+    def parseJValue(input: JValue): JSONQueryASTValue = (input, input \ "#type", input \ "#value") match {
+        case (input, JString("value"), value) =>
+            parseJValue(value)
+        case (input, JString("property"), propertyArray: JArray) =>
+            val properties = propertyArray.arr map {
+                case JString(s) => Left(s)
+                case JInt(i) => Right(i)
+            }
+            JSONQueryASTProperty(properties)
+        case (input, JString("operation"), operandArray: JArray) =>
+            val (operation, operands) = input \ "#operation" match {
+                case JString(s) => (s, operandArray.arr map parseJValue)
+                case _ => (operandArray.arr.head match {
+                    case JString(s) => s
+                }, operandArray.arr.tail map parseJValue)
+            }
+            JSONQueryASTOperation(operation, operands)
+        case (JNothing, _, _) => JSONQueryASTNothing
+        case (JNull, _, _) => JSONQueryASTNull
+        case (JString(s), _, _) => JSONQueryASTString(s)
+        case (JDouble(d), _, _) => JSONQueryASTDouble(d)
+        case (JInt(i), _, _) => JSONQueryASTInt(i)
+        case (JBool(b), _, _) => JSONQueryASTBool(b)
+        case (JField(k, v), _, _) => JSONQueryASTField(k, parseJValue(v))
+        case (JObject(o), _, _) => JSONQueryASTObject(o map { case JField(k, v) => JSONQueryASTField(k, parseJValue(v)) })
+        case (JArray(a), _, _) => JSONQueryASTArray(a map parseJValue)
     }
 }
 
-class JSONQueryAST
 
-case object JSONQueryASTNothing extends JSONQueryAST
-
-case class JSONQueryASTValue(value: JValue) extends JSONQueryAST
-
-case class JSONQueryASTProperty(property: Seq[Either[String, BigInt]]) extends JSONQueryAST
-
-case class JSONQueryASTOperation(operation: String, operands: Seq[JSONQueryAST]) extends JSONQueryAST
